53900dad03281ffc6e64a68c97a3ba201dcbf58d
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-09-16 01:34:11 +0900

    Add tmp p-tuning implementation

---
+++ Attention/decoder_model/p_tuning.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PromptEncoder(nn.Module):
+    """
+    This class is implemented P-tuning for boost LLM's NLU performance
+    Args:
+
+    References:
+        https://arxiv.org/abs/2103.10385
+    """
+    def __init__(self) -> None:
+        pass
