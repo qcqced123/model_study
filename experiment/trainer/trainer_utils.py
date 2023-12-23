fd1ed4059864cc2933b84a9e2c8d6642a230c07c
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-12-23 22:27:58 +0900

    Update trainer_utils.py

--- experiment/trainer/trainer_utils.py
+++ experiment/trainer/trainer_utils.py
@@ -1,22 +1,37 @@
-import numpy as np
+import experiment.configuration as configuration
 import torch
+import torch.nn as nn
+import numpy as np
 import transformers
 import more_itertools
 from torch.utils.data import Sampler, Dataset, DataLoader
 from torch import Tensor
-from typing import List, Tuple, Dict
-from utils.helper import seed_worker
+from typing import List, Tuple, Dict, Type, Union
+from experiment.utils.helper import seed_worker
 
 
-def get_optimizer_grouped_parameters(model, layerwise_lr, layerwise_weight_decay, layerwise_lr_decay):
-    """ Grouped Version: Layer-wise learning rate decay """
+def get_optimizer_grouped_parameters(
+    model: nn.Module,
+    layerwise_lr: float,
+    layerwise_weight_decay: float,
+    layerwise_lr_decay: float
+) -> List[Dict[str, float]]:
+    """ Grouped Version Layer-wise learning rate decay
+    1) initialize lr for task specific layer
+    2) initialize lr for every layer
+    Args:
+        model: model instance from customizing model
+        layerwise_lr: learning rate for task specific layer
+        layerwise_weight_decay: weight decay for task specific layer
+        layerwise_lr_decay: learning rate decay for every layer
+    """
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    # initialize lr for task specific layer
-    optimizer_grouped_parameters = [{"params": [p for n, p in model.named_parameters() if "model" not in n],
-                                     "weight_decay": 0.0,
-                                     "lr": layerwise_lr,
-                                     }, ]
-    # initialize lrs for every layer
+    optimizer_grouped_parameters = [
+        {"params": [p for n, p in model.named_parameters() if "model" not in n],
+         "weight_decay": 0.0,
+         "lr": layerwise_lr
+         },
+    ]
     layers = [model.model.embeddings] + list(model.model.encoder.layer)
     layers.reverse()
     lr = layerwise_lr
@@ -29,43 +44,35 @@ def get_optimizer_grouped_parameters(model, layerwise_lr, layerwise_weight_decay
             {"params": [p for n, p in layer.named_parameters() if any(nd in n for nd in no_decay)],
              "weight_decay": 0.0,
              "lr": lr,
-             },]
+             },
+        ]
         lr *= layerwise_lr_decay
     return optimizer_grouped_parameters
 
 
-def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay):
-    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    optimizer_parameters = [
-        {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay)],
-         'lr': encoder_lr, 'weight_decay': weight_decay},
-        {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay)],
-         'lr': encoder_lr, 'weight_decay': 0.0},
-        {'params': [p for n, p in model.named_parameters() if "model" not in n],
-         'lr': decoder_lr, 'weight_decay': 0.0}
-    ]
-    return optimizer_parameters
-
-
-def collate(inputs) -> Dict:
-    """ Descending sort inputs by length of sequence """
+def collate(inputs: Dict) -> Dict:
+    """ Descending sort inputs by length of sequence
+    Args:
+        inputs: inputs from torch.utils.data.DataLoader, which is from transformers.AutoTokenizer
+    """
     mask_len = int(inputs["attention_mask"].sum(axis=1).max())
     for k, v in inputs.items():
         inputs[k] = inputs[k][:, :mask_len]
     return inputs
 
 
-def one2many_collate(inputs, label1, label2) -> Tuple[Dict, Tensor, Tensor]:
-    """ Descending sort inputs by length of sequence """
-    mask_len = int(inputs["attention_mask"].sum(axis=1).max())
-    for k, v in inputs.items():
-        inputs[k] = inputs[k][:, :mask_len]
-    label1, label2 = label1[:, :mask_len], label2[:, :mask_len]
-    return inputs, label1, label2
-
-
-def get_dataloader(cfg, dataset: Dataset, generator: torch.Generator, shuffle: bool = True, collate_fn=None, sampler=None, drop_last: bool = True) -> DataLoader:
-    """ function for initializing torch.utils.data.DataLoader Module """
+def get_dataloader(
+    cfg: configuration.CFG,
+    dataset: Dataset,
+    generator: torch.Generator,
+    shuffle: bool = True,
+    collate_fn=None,
+    sampler=None,
+    drop_last: bool = True
+) -> DataLoader:
+    """ function for initializing torch.utils.data.DataLoader Module
+    All Args are from torch.nn.utils.data.DataLoader except cfg
+    """
     dataloader = DataLoader(
             dataset,
             batch_size=cfg.batch_size,
@@ -77,23 +84,21 @@ def get_dataloader(cfg, dataset: Dataset, generator: torch.Generator, shuffle: b
             num_workers=cfg.num_workers,
             pin_memory=True,
             drop_last=drop_last,
-        )
+    )
     return dataloader
 
 
-def get_swa_scheduler(cfg, optimizer):
-    """  SWA Scheduler """
-    swa_scheduler = getattr(torch.optim.swa_utils, 'SWALR')(
-        optimizer,
-        swa_lr=cfg.swa_lr,
-        anneal_epochs=cfg.anneal_epochs,
-        anneal_strategy=cfg.anneal_strategy
-    )
-    return swa_scheduler
-
-
-def get_scheduler(cfg, optimizer, len_train: int):
-    """ Select Scheduler Function """
+def get_scheduler(
+    cfg: configuration.CFG,
+    optimizer: torch.optim.Optimizer,
+    len_train: int
+) -> torch.optim.lr_scheduler:
+    """ Select Scheduler Function (cosine_annealing, cosine, linear)
+    Args:
+        cfg: for getting scheduler options from configuration.py
+        optimizer: torch.optim.Optimizer
+        len_train: length of training dataset for calculating total steps
+    """
     scheduler_dict = {
         'cosine_annealing': 'get_cosine_with_hard_restarts_schedule_with_warmup',
         'cosine': 'get_cosine_schedule_with_warmup',
@@ -108,8 +113,28 @@ def get_scheduler(cfg, optimizer, len_train: int):
     return lr_scheduler
 
 
-def get_name(cfg) -> str:
-    """ get name of model """
+def get_swa_scheduler(
+    cfg: configuration.CFG,
+    optimizer: torch.optim.Optimizer
+) -> torch.optim.swa_utils.SWALR:
+    """ SWA Scheduler
+    Init scheduler for Stochastic Weight Averaging
+    All Args are from torch.optim.swa_utils.SWALR except cfg
+    Args:
+        cfg: for getting SWA scheduler options from configuration.py
+        optimizer: torch.optim.swa_utils.SWALR
+    """
+    swa_scheduler = getattr(torch.optim.swa_utils, 'SWALR')(
+        optimizer,
+        swa_lr=cfg.swa_lr,
+        anneal_epochs=cfg.anneal_epochs,
+        anneal_strategy=cfg.anneal_strategy
+    )
+    return swa_scheduler
+
+
+def get_name(cfg: configuration.CFG) -> str:
+    """ get name of model for recording experiment result """
     try:
         name = cfg.model.replace('/', '-')
     except ValueError:
@@ -170,7 +195,7 @@ class SmartBatchingCollate:
         self._max_length = max_length
         self._pad_token_id = pad_token_id
 
-    def __call__(self, batch: Tuple) -> Tuple[Tensor, Tensor]:
+    def __call__(self, batch: Tuple) -> Tuple[Dict, Tensor]:
         if self._labels is not None:
             sequences, targets = list(zip(*batch))
         else:
@@ -224,7 +249,7 @@ class MiniBatchCollate(object):
     def __init__(self, batch: torch.utils.data.DataLoader) -> None:
         self.batch = batch
 
-    def __call__(self) -> tuple[dict[Tensor, Tensor, Tensor], Tensor, Tensor]:
+    def __call__(self) -> Tuple[Dict[Tensor, Tensor, Tensor], Tensor, Tensor]:
         inputs, labels, position_list = self.batch
         labels = torch.nn.utils.rnn.pad_sequence(
             labels,
@@ -242,18 +267,26 @@ class MiniBatchCollate(object):
 class AWP:
     """
     Adversarial Weight Perturbation for OneToOne Trainer
+    Args:
+        model: model instance from customizing model
+        criterion: loss function for training, you must pass instance which is inheritance of torch.nn.Module
+        optimizer: optimizer for training, you must pass instance which is inheritance of torch.optim or transformers.optimization
+        apex: if you use apex, you must pass True
+        adv_param: parameter name for adversarial weight perturbation, default is 'weight'
+        adv_lr: learning rate for adversarial weight perturbation, default is 1.0
+        adv_eps: epsilon for adversarial weight perturbation, default is 0.01
     References:
         https://www.kaggle.com/code/skraiii/pppm-tokenclassificationmodel-train-8th-place
     """
     def __init__(
         self,
-        model,
-        criterion,
-        optimizer,
-        awp: bool,
+        model: nn.Module,
+        criterion: Type[nn.Module],
+        optimizer: Union[torch.optim.Optimizer, torch.optim.swa_utils.SWALR, transformers.optimization],
+        apex: bool,
         adv_param: str = "weight",
-        adv_lr: float=1.0,
-        adv_eps: float=0.01
+        adv_lr: float = 1.0,
+        adv_eps: float = 0.01
     ) -> None:
         self.model = model
         self.criterion = criterion
@@ -261,21 +294,21 @@ class AWP:
         self.adv_param = adv_param
         self.adv_lr = adv_lr
         self.adv_eps = adv_eps
-        self.awp = awp
+        self.apex = apex
         self.backup = {}
         self.backup_eps = {}
 
-    def attack_backward(self, inputs: Dict, c_label: Tensor, w_label: Tensor):
-        with torch.cuda.amp.autocast(enabled=self.awp):
+    def attack_backward(self, inputs: dict, label: Tensor) -> Tensor:
+        with torch.cuda.amp.autocast(enabled=self.apex):
             self._save()
             self._attack_step()
-            pred_list = self.model(inputs)
-            c_pred, w_pred = pred_list[:, 0], pred_list[:, 1]
-            c_adv_loss, w_adv_loss = self.criterion(c_pred, c_label), self.criterion(w_pred, w_label)
-            # mask = (label.view(-1, 1) != -1)  # this line will be needed for OneToMany Trainer
-            # adv_loss = torch.masked_select(adv_loss, mask).mean()  # this line will be needed for OneToMany Trainer
+            y_preds = self.model(inputs)
+            adv_loss = self.criterion(
+                y_preds.view(-1, 1), label.view(-1, 1))
+            mask = (label.view(-1, 1) != -1)
+            adv_loss = torch.masked_select(adv_loss, mask).mean()
             self.optimizer.zero_grad()
-        return c_adv_loss, w_adv_loss
+        return adv_loss
 
     def _attack_step(self) -> None:
         e = 1e-6
@@ -311,7 +344,10 @@ class AWP:
 
 
 class AverageMeter(object):
-    """ Computes and stores the average and current value """
+    """ Computes and stores the average and current value
+    Reference:
+        https://github.com/pytorch/examples/blob/main/imagenet/main.py
+    """
     def __init__(self):
         self.reset()
 
@@ -329,18 +365,16 @@ class AverageMeter(object):
 
 
 class EarlyStopping(object):
-    """
-    Monitor a metric and stop training when it stops improving.
-
+    """ Monitor a metric and stop training when it stops improving.
     Args:
         mode: 'min' for loss base val_score for loss, 'max' for metric base val_score
         patience: number of checks with no improvement, default = 3
         min_delta: minimum change in the monitored quantity to qualify as an improvement, i.e. an absolute
             change of less than or equal to `min_delta`, will count as no improvement. default = 0.0
         detect_anomaly: When set ``True``, stops training when the monitor becomes NaN or infinite, etc
-                        default = True
+            default = True
     """
-    def __init__(self, mode: str, patience: int = 3, min_delta: float = 0.0, detect_anomaly: bool = True):
+    def __init__(self, mode: str, patience: int = 3, min_delta: float = 0.0, detect_anomaly: bool = True) -> None:
         self.mode = mode
         self.early_stop = False
         self.patience = patience
@@ -348,7 +382,6 @@ class EarlyStopping(object):
         self.counter = 0
         self.detect_anomaly = detect_anomaly
         self.val_score = -np.inf
-
         if self.mode == 'min':
             self.val_score = np.inf
 
@@ -357,7 +390,7 @@ class EarlyStopping(object):
         torch.autograd.set_detect_anomaly(self.detect_anomaly)
         return
 
-    def __call__(self, score: any) -> None:
+    def __call__(self, score: float) -> None:
         """ When call by Trainer Loop, Check Trainer need to early stopping """
         if self.mode == 'min':
             if self.val_score >= score:
