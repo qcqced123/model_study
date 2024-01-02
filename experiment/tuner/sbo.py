6e91824c36fc3f4cc370f4de2a493faa6d4e13b7
JungHwan Lee <qcqced123@gmail.com>
2024-01-02 20:07:29 +0900

    fix rel url

--- experiment/tuner/sbo.py
+++ experiment/tuner/sbo.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
 from torch import Tensor
 from typing import Dict, List, Tuple, Union, Optional, Any
-from mlm import PretrainingMaskingCollator
+from ..tuner.mlm import PretrainingMaskingCollator
 from configuration import CFG
 
 
