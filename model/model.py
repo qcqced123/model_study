need to fix distilbert masking flow

model/model.py
@@ -285,7 +285,7 @@ class DistillationKnowledge(nn.Module, AbstractTask):
         last_hidden_state = torch.masked_select(last_hidden_state, mask)
         last_hidden_state = last_hidden_state.view(-1, self.cfg.dim_model)  # flatten last_hidden_state
         soft_target = F.softmax(
-            t_logit.view(-1, self.cfg.vocab_size) / temperature,  # flatten softmax distribution
+            t_logit.view(-1, self.cfg.vocab_size) / temperature**2,  # flatten softmax distribution
             dim=-1
         )  # [bs* seq, vocab_size]
         return last_hidden_state, soft_target
@@ -309,7 +309,7 @@ class DistillationKnowledge(nn.Module, AbstractTask):
         last_hidden_state = last_hidden_state.view(-1, self.cfg.dim_model)  # flatten last_hidden_state
         c_labels = last_hidden_state.new(last_hidden_state.size(0)).fill_(1)
         soft_pred = F.softmax(
-            s_logit.view(-1, self.cfg.vocab_size) / temperature,  # flatten softmax distribution
+            s_logit.view(-1, self.cfg.vocab_size) / temperature**2,  # flatten softmax distribution
             dim=-1
         )
         return last_hidden_state, s_logit, soft_pred, c_labels

trainer/trainer.py
@@ -1204,7 +1204,7 @@ class DistillKnowledgeTuner(PreTrainTuner):
                     mask=mask
                 )
                 d_loss = criterion["KLDivLoss"](soft_pred.log(), soft_target) * self.cfg.alpha_distillation  # nn.KLDIVLoss
-                s_loss = criterion["CrossEntropyLoss"](s_logit.view(-1, self.cfg.vocab_size), labels.view(-1)) * self.cfg.alpha_student # nn.CrossEntropyLoss
+                s_loss = criterion["CrossEntropyLoss"](s_logit.view(-1, self.cfg.vocab_size), labels.view(-1)) * self.cfg.alpha_student  # nn.CrossEntropyLoss
                 c_loss = criterion["CosineEmbeddingLoss"](s_hidden_state, t_hidden_state, c_labels) * self.cfg.alpha_cosine  # nn.CosineEmbeddingLoss
                 loss = d_loss + s_loss + c_loss  # linear combination loss
 