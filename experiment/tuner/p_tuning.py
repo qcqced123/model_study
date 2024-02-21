f0900c0887c06b641f5d073b2c263d83b2411db4
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-02-21 20:02:13 +0900

    Update p_tuning.py

--- experiment/tuner/p_tuning.py
+++ experiment/tuner/p_tuning.py
@@ -1,12 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from configuration import CFG
 from torch import Tensor
 
 
 class PromptEncoder(nn.Module):
     """
-    This class is implemented P-tuning for boost LLM's NLU performance in fine-tuning
+    This class is implemented P-tuning for boost LLM's NLU performance and speed in fine-tuning
+
+    Args:
+        c_src: tensor index of context span's start position
+        c_end: tensor index of context span's end position
 
     Notes:
         1) nn.Dropout(p=dropout): because this module will be used for instead of nn.Embedding Layer,
@@ -14,18 +20,25 @@ class PromptEncoder(nn.Module):
         2) nn.GELU(): In original paper, author use RELU() but we use GELU because of common sense
 
     Maths:
-        1) bi-lstm(x[0:t], x[t+1:])
-
+        1) Bi-LSTM(h[0:i]), Bi-LSTM(h[i+1:])
+        2)
     References:
         https://arxiv.org/abs/2103.10385
     """
-    def __init__(self, dim_model: int = 1024, dropout: float = 0.1) -> None:
+    def __init__(self, cfg: CFG, c_src: int, c_end: int, dim_model: int = 1024, dropout: float = 0.1) -> None:
         super(PromptEncoder, self).__init__()
+        self.cfg = cfg
+        self.context_src = c_src
+        self.context_end = c_end
+        self.vocab_size = len(self.cfg.tokenizer)
+        self.dim_model = dim_model
+        self.prompt_embedding = nn.Embedding(self.vocab_size, dim_model)
         self.prompt_encoder = nn.LSTM(
             dim_model,
             dim_model // 2,
             num_layers=2,
-            bidirectional=True
+            bidirectional=True,
+            dropout=dropout,
         )
         self.mlp = nn.Sequential(
             nn.Linear(dim_model, dim_model),
