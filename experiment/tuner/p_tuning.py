1e46a15e6d619e79ebf2e3ff3b337144975f92cd
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-02-21 18:55:28 +0900

    Update p_tuning.py

--- experiment/tuner/p_tuning.py
+++ experiment/tuner/p_tuning.py
@@ -6,36 +6,42 @@ from torch import Tensor
 
 class PromptEncoder(nn.Module):
     """
-    This class is implemented P-tuning for boost LLM's NLU performance
-    Args:
-        inputs: input prompt, which is a sequence of tokens
-        masking: masking for select pseudo token, which is param subjected to optimization problem
+    This class is implemented P-tuning for boost LLM's NLU performance in fine-tuning
+
     Notes:
-        nn.Dropout(p=dropout): because this module will be used for instead of nn.Embedding Layer,
+        1) nn.Dropout(p=dropout): because this module will be used for instead of nn.Embedding Layer,
                                in common sense, we do not apply Dropout to Inputs Embedding Layer
+        2) nn.GELU(): In original paper, author use RELU() but we use GELU because of common sense
+
+    Maths:
+        1) bi-lstm(x[0:t], x[t+1:])
+
     References:
         https://arxiv.org/abs/2103.10385
     """
-    def __init__(self, inputs: Tensor, masking: Tensor, dim_model: int = 1024, dim_mlp: int = 4096, dropout: float = 0.1) -> None:
+    def __init__(self, dim_model: int = 1024, dropout: float = 0.1) -> None:
         super(PromptEncoder, self).__init__()
-        self.inputs = inputs  # for entire input prompt
-        self.masking = masking  # for masking select pseudo token
-        self.prompt_encoder = nn.Sequential(
-            nn.LSTM(
-                dim_model,
-                dim_model // 2,
-                num_layers=1,
-                bidirectional=True
-            ),
-            nn.Linear(dim_model, dim_mlp),
+        self.prompt_encoder = nn.LSTM(
+            dim_model,
+            dim_model // 2,
+            num_layers=2,
+            bidirectional=True
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(dim_model, dim_model),
             nn.GELU(),
-            nn.Linear(dim_mlp, dim_model),
-            nn.GELU(),
+            nn.Linear(dim_model, dim_model),
         )
 
-    def forward(self) -> Tensor:
-        inputs = torch.masked_select(self.inputs, self.masking)
-        embeddings = self.prompt_encoder(inputs)
-        return embeddings
+    def forward(self, inputs: Tensor, mask: Tensor) -> Tensor:
+        """
+        Args:
+            inputs: input prompt, which is a sequence of tokens
+            mask: masking for select pseudo token, which is param subjected to optimization problem
+        """
+        x = torch.masked_select(inputs, mask)
+        x = self.prompt_encoder(x)
+        p_h = self.mlp(x)
+        return p_h
 
 
