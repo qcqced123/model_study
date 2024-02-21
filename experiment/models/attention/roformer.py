13ef6d0c9e8c949634acf9e54de501be54623490
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-02-21 18:59:02 +0900

    update

---
+++ experiment/models/attention/roformer.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from experiment.models.abstract_model import AbstractModel
+from torch import Tensor
+from typing import Tuple, List
+from einops.layers.torch import Rearrange
+from configuration import CFG
+
+
+class RoFormer(nn.Module, AbstractModel):
+    pass
+
