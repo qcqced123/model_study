cf4ccec66d44da988bc10865b324a1ba3c4f05c2
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-06-19 12:58:37 +0900

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,23 @@
 import torch, os, sys, random, json
 import numpy as np
 from pathlib import Path
+from typing import List
+
+
+def calculate_layerwise_lr(max_lr: float, num_layers: int, lr_decay: float) -> List[float]:
+    """ function for calculating the layerwise learning rate
+    this function return the lr for each layer in the model, which is decreasing by the lr_decay rate
+
+    Args:
+        max_lr: float, maximum learning rate for the model, lr at the top(last) of the given models
+        num_layers: int, number of layers in the model
+        lr_decay: float, decay rate for the learning rate, this value means that decay rate of the learning rate per layer
+
+    Returns:
+        List[float]: list of learning rates for each layer in the model
+
+    """
+    return [max_lr * (lr_decay ** i) for i in range(num_layers)][::-1]
 
 
 def select_model_file(base_path: str = 'models', arch: str = None, model: str = None) -> str:
@@ -63,3 +80,6 @@ def seed_worker(worker_id) -> None:
     random.seed(worker_seed)
 
 
+if __name__ == '__main__':
+    for i, lr in enumerate(calculate_layerwise_lr(6e-4, 24, 0.95)):
+        print(f"{i}-th layer lr: {lr}")
